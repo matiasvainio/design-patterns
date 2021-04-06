3b4d0ab47e5a37beb6406d02f38c2a6445b0c4e0
Matias Vainio <matias.s.vainio@gmail.com>
2021-04-06 15:22:57 +0300

    unnecessary override methods commented out

--- 6/src/main/java/decorator/DecryptedData.java
+++ 6/src/main/java/decorator/DecryptedData.java
@@ -1,33 +1,57 @@
 package decorator;
 
+/**
+ * Decrypted data class. This extends DataDecorator abstract class.
+ */
 public class DecryptedData extends DataDecorator {
 
     protected DecryptedData(Data decoratedData) {
         super(decoratedData);
     }
 
+    /**
+     * Uses setData method to save decrypted string to object. AES.decrypt is static method which decrypts
+     * the string to plain text.
+     */
     public void decrypt() {
         this.setData(AES.decrypt(this.getData(), Main.SECRET_KEY));
     }
 
+    /**
+     * Decrypts data and uses parent class' write method to write string to a text file.
+     */
     @Override
     public void write() {
         decrypt();
         super.write();
     }
 
-    @Override
-    public String read() {
-        return super.read();
-    }
-
-    @Override
-    public String getData() {
-        return super.getData();
-    }
-
-    @Override
-    public void setData(String data) {
-        super.setData(data);
-    }
+//    Nämähän ovat tietysti turhia, koska toiminnallisuus on luotu jo abstraktissa yliluokassa.
+//
+//    /**
+//     * Reads data from file using parent class' read method.
+//     * @return String data read from file.
+//     */
+//    @Override
+//    public String read() {
+//        return super.read();
+//    }
+//
+//    /**
+//     * Returns data stored to data object in parent class.
+//     * @return String data saved to object.
+//     */
+//    @Override
+//    public String getData() {
+//        return super.getData();
+//    }
+//
+//    /**
+//     * Setter for data. Uses parent class' setData method.
+//     * @param data String data to be set.
+//     */
+//    @Override
+//    public void setData(String data) {
+//        super.setData(data);
+//    }
 }
--- 6/src/main/java/decorator/EncryptedData.java
+++ 6/src/main/java/decorator/EncryptedData.java
@@ -1,33 +1,57 @@
 package decorator;
 
+/**
+ * Decrypted data class. This extends DataDecorator abstract class.
+ */
 public class EncryptedData extends DataDecorator {
 
     protected EncryptedData(Data decoratedData) {
         super(decoratedData);
     }
 
+    /**
+     * Uses setData method to save encrypted string to object. AES.encrypt is static method which encrypts
+     * the string to plain text.
+     */
     public void encrypt() {
         this.setData(AES.encrypt(this.getData(), Main.SECRET_KEY));
     }
 
+    /**
+     * Decrypts data and uses parent class' write method to write string to a text file.
+     */
     @Override
     public void write() {
         encrypt();
         super.write();
     }
 
-    @Override
-    public String read() {
-        return super.read();
-    }
-
-    @Override
-    public String getData() {
-        return super.getData();
-    }
-
-    @Override
-    public void setData(String data) {
-        super.setData(data);
-    }
+//    Nämähän ovat tietysti turhia, koska toiminnallisuus on luotu jo abstraktissa yliluokassa.
+//
+//    /**
+//     * Reads data from file using parent class' read method.
+//     * @return String data read from file.
+//     */
+//    @Override
+//    public String read() {
+//        return super.read();
+//    }
+//
+//    /**
+//     * Returns data stored to data object in parent class.
+//     * @return String data saved to object.
+//     */
+//    @Override
+//    public String getData() {
+//        return super.getData();
+//    }
+//
+//    /**
+//     * Setter for data. Uses parent class' setData method.
+//     * @param data String data to be set.
+//     */
+//    @Override
+//    public void setData(String data) {
+//        super.setData(data);
+//    }
 }
